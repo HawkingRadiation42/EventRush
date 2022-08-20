a9004b58ec08853f0ed06ab02f0660040edb0b8d
Shrey <thisisshreykhandelwal@protonmail.com>
2022-08-20 20:31:13 +0000

    mongodb

--- server/database.py
+++ server/database.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 #import motor.motor_asyncio
 
 client = MongoClient("mongodb+srv://hawkingradiation:1234@cluster0.y1uabs9.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://hawkingradiation:1234@cluster0.y1uabs9.mongodb.net/?retryWrites=true&w=majority")
+
 db = client.event_rush
 #client = motor.motor_asyncio.AsyncIOMotorClient(client, 27017)
 

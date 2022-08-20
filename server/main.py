d6afec987fb7bd11500124b32b8a974cdabeb1f1
Shrey <shrey.209303098@muj.manipal.edu>
2022-08-21 00:38:10 +0530

    updating main.py to resolve merge conflicts

--- server/main.py
+++ server/main.py
@@ -1,19 +1,22 @@
 from http.client import HTTPResponse
-
+from signal import sigpending
+from urllib import response
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
-from model import SignUp, Login
+from fastapi.responses import HTMLResponse, RedirectResponse
+from model import SignUp, Login, Register
 
 
 from database import signin
-from database import register 
+from database import register
+from database import register_event
 
 
 app = FastAPI()
 
 
-origins = ['https://localhost:3000']
+origins = ['*']
 
 
 app.add_middleware(
@@ -30,13 +33,42 @@ async def registering_signup(signup: SignUp):
     response = await register(signup.dict())
     if response:
         print("post successfull!")
-        return response
+        return 
     raise HTTPException(400, "something went wrong/bad request")
     
 
-@app.post("/login", response_model=SignUp, status_code=200)
+@app.post("/login", response_model=SignUp, status_code=200) #removed response model as data not fetched from the database 
 async def register_login(login:Login):
-    response = await signin(login.dict())
+    # details = login.dict()
+    # Email = login.email
+    response = await signin(login)
     if response:
-        return response 
-    raise HTTPException(400, "something went wrong/bad request")
+        return 
+    # raise HTTPException(400, f"something went wrong/bad request {}")
+
+
+
+# @app.post("/cultural", response_model=cultural_events, status_code=200)
+# async def coding_events_func(event: cultural_events):
+#     response = await register_cultural_events(event)
+#     if response:
+#         return response
+#     raise HTTPException(404,"there are no events to register")
+
+
+# TO SEND ID TO FRONTEND USING RESPONSE
+
+# @app.post("/cultural", status_code=200)
+# async def event_id(event:id):
+#     response = await event_id(event)
+#     if response:
+#         return response
+#     raise HTTPException(404, "there are no events to register")
+
+
+@app.post("/registeration",status_code=200)
+async def registeration_event(register: Register):
+    response = await register_event(register.dict())
+    if response:
+        return 
+    raise HTTPException(400, "something went wrong / bad request")

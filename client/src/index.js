3f1d4ceb8e2722f331df1e5379dfcafb6eb5aa8c
Sayantan <sayantan.dps@gmail.com>
2022-08-20 13:55:07 +0530

    Additions to Sign Up

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SnackbarProvider
+      maxSnack={1}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      preventDuplicate
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SnackbarProvider>
   </React.StrictMode>
 );

e54709ee492c53cdf301767c639691d5cd4dfcd2
Samridh Srivastava <Samridh@gmail.com>
2022-08-21 00:19:02 +0530

    Oneiros Page added

--- client/src/pages/Oneiros.jsx
+++ client/src/pages/Oneiros.jsx
@@ -3,14 +3,15 @@ import Navbar from "../components/Navbar";
 import footer from "../assets/footer.svg";
 import OneirosComp from "../components/OneirosComp";
 
+
 const Oneiros = () => {
   return (
     <div>
-      <Navbar />
-      <div className="bg-stone-100">
-        <OneirosComp />
-      </div>
-      <img src={footer} alt="footer" className=" h-3/5" />
+      <Navbar />      
+      <OneirosComp />
+      <center>
+      <img src={footer} alt="footer" className="w-11/12 h-3/5" />
+      </center>
     </div>
   );
 };

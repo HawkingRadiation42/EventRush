d060c53cf51cce0d3e6c63a2a2795fbd5ff8bb08
Sayantan <sayantan.dps@gmail.com>
2022-08-20 12:05:48 +0530

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 };

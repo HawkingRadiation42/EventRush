a74f00575811f8d1f2c37702838cb15a322501a8
Sayantan <sayantan.dps@gmail.com>
2022-08-20 13:03:48 +0530

    Signup additions

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./SignUp.css";
 import upload from "../assets/UploadPhoto.svg";
+import { MdAlternateEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import { BsGenderAmbiguous } from "react-icons/bs";
 
 const SignUp = () => {
   return (
@@ -10,18 +14,72 @@ const SignUp = () => {
           <h1>Event Rush</h1>
           <h2>Sign up for CampusRush</h2>
           <form>
+            <div className="mb-6">
+              <label
+                for="email"
+                className="block text-sm font-medium text-gray-900 "
+              >
+                Your Email
+              </label>
+              <div className="flex items-center border rounded-lg focus:border-blue-500">
+                <MdAlternateEmail className="text-gray-600" />
+                <input
+                  type="email"
+                  id="email"
+                  className="  text-gray-900 text-sm rounded-lg focus:outline-none  block w-full p-2.5 "
+                  placeholder="john.doe@company.com"
+                  required=""
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Password
+              </label>
+              <div className="flex items-center border rounded-lg focus:border-blue-500">
+                <RiLockPasswordFill className="text-gray-600" />
+                <input
+                  type="password"
+                  id="password"
+                  className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-none block w-full p-2.5 "
+                  placeholder="•••••••••"
+                  required=""
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Name
+              </label>
+              <div className="flex items-center border focus:border-blue-500 rounded-lg">
+                <FaUserAlt className="text-gray-600" />
+                <input
+                  type="text"
+                  id="name"
+                  className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-none block w-full p-2.5 "
+                  required=""
+                />
+              </div>
+            </div>
             <div className="grid gap-6 mb-6 md:grid-cols-2">
               <div>
                 <label
                   for="first_name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
-                  First name
+                  Age
                 </label>
+
                 <input
                   type="text"
                   id="first_name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                  className=" border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                   placeholder="John"
                   required=""
                 />
@@ -31,62 +89,51 @@ const SignUp = () => {
                   for="last_name"
                   className="block mb-2 text-sm font-medium text-gray-900 "
                 >
-                  Last name
+                  Gender
                 </label>
-                <input
-                  type="text"
-                  id="last_name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                  placeholder="Doe"
-                  required=""
-                />
+                <div className="flex items-center border focus:border-blue-500 rounded-lg">
+                  <BsGenderAmbiguous className="text-gray-600" />
+                  <input
+                    type="text"
+                    id="last_name"
+                    className=" text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:outline-none block w-full p-2.5 "
+                    placeholder="Doe"
+                    required=""
+                  />
+                </div>
               </div>
             </div>
-            <div className="mb-6">
-              <label
-                for="email"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Email address
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                placeholder="john.doe@company.com"
-                required=""
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                for="password"
-                className="block mb-2 text-sm font-medium text-gray-900 "
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                placeholder="•••••••••"
-                required=""
-              />
-            </div>
+
             <div className="mb-6">
               <label
                 for="confirm_password"
                 className="block mb-2 text-sm font-medium text-gray-900 "
               >
-                Confirm password
+                College/University Name
               </label>
               <input
-                type="password"
-                id="confirm_password"
+                type="text"
+                id="college"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                placeholder="•••••••••"
+                placeholder=""
                 required=""
               />
             </div>
+            <div className="mb-2">
+              <label
+                for="email"
+                className="block text-sm font-medium text-gray-900 "
+              >
+                College Email Id
+              </label>
+            </div>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="john.doe@company.com"
+              required=""
+            />
             <div className="flex items-start mb-6">
               <div className="flex items-center h-5">
                 <input
@@ -122,7 +169,10 @@ const SignUp = () => {
         <div>
           <hr />
         </div>
-        <img src={upload} />
+        <div className="flex flex-col">
+          <img src={upload} alt="img" />
+          <p className="text-center">Upload your Profile Picture Above</p>
+        </div>
       </div>
     </div>
   );

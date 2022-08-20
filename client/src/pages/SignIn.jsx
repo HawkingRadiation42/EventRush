46c8c189e4d442c86c770f87f76390461d1261b4
Samridh Srivastava <Samridh@gmail.com>
2022-08-20 13:02:50 +0530

    login-layout-created

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,21 +4,82 @@ import "./SignIn.css";
 const SignIn = () => {
   return (
     <div className="back flex justify-center items-center">
-      <div className="flex justify-center items-center bg-white">
-        <div>
-          <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Sign In to Your Account</h2>
+    <div className="flex justify-center items-center bg-white">
+      <div className="mt-6 w-1/2 h-full flex justify-center flex-col items-center">
 
-        </div>
-
-        <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="text" name="remember" value="true" id="" />
+        <h1 className="mb-6">Event Rush</h1>
+        <h2>Sign in to your Account</h2>
+        <form className="flex flex-col justify-center items-center h-96 w-96">
+          <div className="mb-6">
+            <label
+              for="email"
+              className="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-auto p-2.5 "
+              placeholder="john.doe@company.com"
+              required=""
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              for="password"
+              className="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="•••••••••"
+              required=""
+            />
+          </div>
+          <div className="mb-6">
+            
+          </div>
+          <div className="flex items-start mb-6">
+            <div className="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 "
+                required=""
+              />
+            </div>
+            <label
+              for="remember"
+              className="ml-2 text-sm font-medium text-gray-900 "
+            >
+              I agree with the{" "}
+              <a
+                href="#"
+                className="text-blue-600 hover:underline dark:text-blue-500"
+              >
+                terms and conditions
+              </a>
+              .
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
         </form>
-        <input>
-
-        </input>
-      
+      </div>
+      <div>
+        <hr />
       </div>
     </div>
+  </div>
   );
 };
 

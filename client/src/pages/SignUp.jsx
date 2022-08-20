3a1d25211c67dbb6ecc6e83954eae2c76b4ebd13
Sayantan <sayantan.dps@gmail.com>
2022-08-20 12:36:11 +0530

    Initial Sign Up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,129 @@
 import React from "react";
 import "./SignUp.css";
+import upload from "../assets/UploadPhoto.svg";
 
 const SignUp = () => {
   return (
     <div className="back flex justify-center items-center">
-      <div className="flex justify-center items-center bg-white">Hello</div>
+      <div className="flex justify-center items-center bg-white">
+        <div className="w-1/2 h-full flex justify-center flex-col items-center">
+          <h1>Event Rush</h1>
+          <h2>Sign up for CampusRush</h2>
+          <form>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+              <div>
+                <label
+                  for="first_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  First name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                  placeholder="John"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="last_name"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Last name
+                </label>
+                <input
+                  type="text"
+                  id="last_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                  placeholder="Doe"
+                  required=""
+                />
+              </div>
+            </div>
+            <div className="mb-6">
+              <label
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="john.doe@company.com"
+                required=""
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                for="password"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="•••••••••"
+                required=""
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                for="confirm_password"
+                className="block mb-2 text-sm font-medium text-gray-900 "
+              >
+                Confirm password
+              </label>
+              <input
+                type="password"
+                id="confirm_password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="•••••••••"
+                required=""
+              />
+            </div>
+            <div className="flex items-start mb-6">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 "
+                  required=""
+                />
+              </div>
+              <label
+                for="remember"
+                className="ml-2 text-sm font-medium text-gray-900 "
+              >
+                I agree with the{" "}
+                <a
+                  href="#"
+                  className="text-blue-600 hover:underline dark:text-blue-500"
+                >
+                  terms and conditions
+                </a>
+                .
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div>
+          <hr />
+        </div>
+        <img src={upload} />
+      </div>
     </div>
   );
 };

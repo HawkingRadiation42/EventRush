3ccaf8a5afb1d554a2004f93f4daae1791350495
Sayantan Mondal <sayantan.dps@gmail.com>
2022-10-06 15:52:33 +0000

    Patches: Removed redundant code and made dynamic changes

--- client/src/components/EventBanner.jsx
+++ client/src/components/EventBanner.jsx
@@ -4,7 +4,7 @@ import "./EventBanner.css";
 const EventBanner = () => {
   return (
     <div className="eventback pt-5">
-      <h1>Hello World</h1>
+      <h1>This is a banner</h1>
     </div>
   );
 };

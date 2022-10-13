c5275fcd445bd35612200e47e679e8c9b9a62f16
Vansh02 <89703025+Vansh02@users.noreply.github.com>
2022-10-13 16:56:34 +0530

    Update Firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: "wildhacks-ii.appspot.com",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 };
